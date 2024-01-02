65fc42069bbe415bbf149eba4cf0908511c16f03
ridhi <87061744+rdhgarg@users.noreply.github.com>
2024-01-02 12:21:54 +0530

    push code

---
+++ app-copy.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+const connection = mysql.createConnection({
+  host: 'learnnprepdb.cuml7mxd9x3f.ap-south-1.rds.amazonaws.com',
+  user: 'learnnprep',
+  password: 'learnnprep2024',
+  database: 'lmsdb',
+  port: 3306,
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to live database:', error);
+    return;
+  }
+  console.log('Connected to live database.');
+});
+
+// Define a route to get all courses
+app.get('/api/courses', (req, res) => {
+  const sqlQuery = 'SELECT * FROM courses';
+
+  connection.query(sqlQuery, (error, results) => {
+    if (error) {
+      console.error('Error executing query:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+
+    res.json({ courses: results });
+  });
+});
+
+app.get('/', (req, res) => {
+  const sqlQuery = 'SELECT * FROM courses';
+
+  console.log('Connected to live database.');
+});
+
+// Start the Express server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- app.js
+++ app.js
@@ -1,37 +1,34 @@
-const express = require('express');
 const mysql = require('mysql');
-
+const express = require('express');
 const app = express();
 
-const connection = mysql.createConnection({
-  host: 'learnnprepdb.cuml7mxd9x3f.ap-south-1.rds.amazonaws.com',
-  user: 'learnnprep',
-  password: 'learnnprep2024',
-  database: 'lmsdb',
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: '68.178.158.76',
+  user: 'gaurang_new',
+  password: 'work@2023',
+  database: 'gaurang_new',
   port: 3306,
 });
 
-connection.connect((error) => {
-  if (error) {
-    console.error('Error connecting to live database:', error);
-    return;
-  }
-  console.log('Connected to live database.');
-});
-
 // Define a route to get all courses
-app.get('/api/courses', (req, res) => {
-  const sqlQuery = 'SELECT * FROM courses';
+app.get('/api/product', (req, res) => {
+  const sqlQuery = 'SELECT * FROM users';
 
-  connection.query(sqlQuery, (error, results) => {
+  pool.query(sqlQuery, (error, results) => {
     if (error) {
       console.error('Error executing query:', error);
       res.status(500).json({ error: 'Internal Server Error' });
       return;
     }
 
-    res.json({ courses: results });
+    res.json({ products: results });
   });
+});
+
+// Define a route for the root endpoint
+app.get('/', (req, res) => {
+  res.send('Hello, this is the root endpoint.');
 });
 
 // Start the Express server
